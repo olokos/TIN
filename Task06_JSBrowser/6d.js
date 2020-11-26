0b1ca599b1464b5e3ac8e6e03c441b03c880b6ee
olokos <olokos@wp.pl>
2020-11-27 00:12:08 +0100

    Completed Task06_JSBrowser

--- Task06_JSBrowser/6d.js
+++ Task06_JSBrowser/6d.js
@@ -25,21 +25,29 @@ function validateNumber(stringToValidate) {
 }
 
 function validateBothFields() {
-    validateNumber(document.getElementById("favoriteNumber").value);
-    validateEmail(document.getElementById("emailField").value);
+    let validatedNumber = validateNumber(document.getElementById("favoriteNumber").value);
+    let validatedEmail = validateEmail(document.getElementById("emailField").value);
+    if (validatedEmail && validatedEmail) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 
 function addNewTableRow() {
-    /*<tr>
-        <td>42069</td>
-        <td>example@email.com</td>
-    </tr>*/
-    let tr = document.createElement("tr");
-    let tdNumber = document.createElement("td");
-    let tdEmail = document.createElement("td");
-    let tdNumberText = document.createTextNode("You need to enter a value into Celsius input and press the button Calculate F*!");
-    let tdEmailText = document.createTextNode("You need to enter a value into Celsius input and press the button Calculate F*!");
-    paragraph.appendChild(paragraphText);
-    document.body.appendChild(paragraph);
+    if (validateBothFields()) {
+        let tr = document.createElement("tr");
+        let tdNumber = document.createElement("td");
+        let tdEmail = document.createElement("td");
+        let tdNumberText = document.createTextNode(document.getElementById("favoriteNumber").value);
+        let tdEmailText = document.createTextNode(document.getElementById("emailField").value);
+        tr.appendChild(tdNumber);
+        tr.appendChild(tdEmail);
+        tdNumber.appendChild(tdNumberText);
+        tdEmail.appendChild(tdEmailText);
+        document.getElementById("rows").append(tr);
+    } else {
+        console.error("Invalid data provided, not appending to table!");
+    }
 }
