785dc15b19e1a9f67849893920f741472c5a683c
olokos <olokos@wp.pl>
2020-12-04 14:25:02 +0100

    Upload completed Task 7 - part A

--- Task07_NodeJSIntro/7a.js
+++ Task07_NodeJSIntro/7a.js
@@ -6,23 +6,33 @@ http.createServer(function(req, res){
     //res.write(req.url);
     //res.write('Hello world!');
     var q = url.parse(req.url, true).query;
-    var requestParameters = "Your query is: " + q.firstNumber + " " + q.operation + " " + q.secondNumber + "<br>";
+    if(isNaN(q.firstNumber)){
+        var requestParameters = "Invalid first number provided: " + q.firstNumber + " is not a number!<br>";
+    }else if(isNaN(q.secondNumber)){
+        var requestParameters = "Invalid second number provided: " + q.secondNumber + " is not a number!<br>";
+    }else{
+        var requestParameters = "Your query is: " + q.firstNumber + " " + q.operation + " " + q.secondNumber + "<br>";
+    }
     res.write(requestParameters);
-    if(q.operation=='add'){
+    if(q.operation=='add' && !isNaN(q.firstNumber) && !isNaN(q.secondNumber)){
         var response = "Addition result: " + q.firstNumber + q.secondNumber;
         res.write(response);
     }
-    if(q.operation=='sub'){
+    else if(q.operation=='sub' && !isNaN(q.firstNumber) && !isNaN(q.secondNumber)){
         var response = "Substraction result: " + q.firstNumber - q.secondNumber;
         res.write(response);
     }
-    if(q.operation=='mul'){
+    else if(q.operation=='mul' && !isNaN(q.firstNumber) && !isNaN(q.secondNumber)){
         var response = "Mutliplication result: " +q.firstNumber * q.secondNumber;
         res.write(response);
     }
-    if(q.operation=='div'){
+    else if(q.operation=='div' && !isNaN(q.firstNumber) && !isNaN(q.secondNumber)){
         var response = "Division result: " +q.firstNumber / q.secondNumber;
+        res.write(response);
+    }else{
+        var response = "Invalid operator or number provided! Not calculating!"
         res.write(response);
     }
     res.end();
 }).listen(8080);
+// to invoke: http://localhost:8080/?operation=add&firstNumber=5&secondNumber=6
