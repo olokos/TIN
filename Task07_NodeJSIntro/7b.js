42a5a92374e9e3e00168800ff128b643a5e3256b
olokos <olokos@wp.pl>
2020-12-07 22:11:00 +0100

    Fixed 7 Stop using hardcoded values

--- Task07_NodeJSIntro/7b.js
+++ Task07_NodeJSIntro/7b.js
@@ -6,15 +6,15 @@ http.createServer(function(req, res){
     res.writeHead(200, {'Content-Type': 'text/html'});
 
     let q = url.parse(req.url, true).query;
-    let wantedFolder = q.wantedFolder;
+    let wantedFolder = './' + q.wantedFolder;
 
-    let testFolder = './someFolder';
-    fs.watch(testFolder, (eventType, filename) => { 
+    
+    fs.watch(wantedFolder, (eventType, filename) => { 
     console.log('The file', filename, 'was modified!'); 
-    console.log(fs.readFile(testFolder+ '/' + filename, 'utf-8', function (err,data){
+    fs.readFile(wantedFolder + '/' + filename, 'utf-8', function (err,data){
         if (err) throw err;
         console.log(data);
-    }));
+    });
   }); 
     res.end();
 }).listen(8080);
